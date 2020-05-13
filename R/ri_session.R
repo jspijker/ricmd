a2e035558a6228d5ffd2a9c0cf9e2009922fdc05
spijker <job.spijker@rivm.nl>
2020-05-13 13:35:20 +0200

    ri_session added

---
+++ R/ri_session.R
@@ -0,0 +1,25 @@
+#' Starts an R - IRODS session
+#'
+#' This functions reads the IRODS environment file and starts and
+#' IRODS session
+#'
+#' @param env name of IRODS environment configuration
+#'
+#' @return nothing
+#'
+#'
+#' @export
+
+
+ri_session <- function(env=path.expand('~/.irods/irods_environment.json')) {
+
+    if(!file.exists(env)) {
+        stop("irods environment file not found")
+    }
+
+    irods <- import("irods.session")
+    session <- irods$iRODSSession(irods_env_file=env)
+    assign("session",session,env=.ricmdEnv)
+
+}
+
---
+++ tests/testthat/test-ri_session.R
@@ -0,0 +1,17 @@
+context("ri_session")
+
+test_that("valid arguments", {
+
+              expect_error(ri_session(1))
+              expect_error(ri_session("nonexistingpathtoenvironmentfile"))
+
+})
+
+test_that("correct function",{
+
+              ri_session()
+              session <- get("session",env=.ricmdEnv)
+              expect_true(typeof(session)=="environment")
+
+})
+
