1bdacde5339e1bb54c9d364cb58d4826434c04cb
spijker <job.spijker@rivm.nl>
2020-06-03 14:33:27 +0200

    Tests now use a specific collection for tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,7 +2,14 @@
 cat("running setup ...")
 
 # testing environment
-testColl <- "/tempZone/home/devel"
+testColl <- "/tempZone/home/devel/tests"
+
+env <- path.expand('~/.irods/irods_environment.json')
+irods <- import("irods.session")
+session <- irods$iRODSSession(irods_env_file=env)
+session$collections$create(testColl)
+rm(session)
+
 
 cat("Done!\n")
 
--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -3,5 +3,11 @@ cat("running teardown ...")
 
 # still empty
 
+    
+irods <- import("irods.session")
+env <- path.expand('~/.irods/irods_environment.json')
+session <- irods$iRODSSession(irods_env_file=env)
+session$collections$remove(testColl)
+rm(session)
 
 cat("Done!\n")
