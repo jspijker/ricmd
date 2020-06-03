ee20c9339c3c701fbd346cc23cd3e35ed2059524
spijker <job.spijker@rivm.nl>
2020-06-03 14:24:52 +0200

    Added ri_put function
    But this function can create a mess of our testing setup, so changes
    needed

---
+++ R/ri_put.R
@@ -0,0 +1,49 @@
+#' Store data object in iRODS collection
+#'
+#' Stores a data object in a iRODS collection. If the data object
+#' allready exists, it is not overwritten by default
+#'
+#' @param filename filename or path of file to store
+#' @param collection if NULL this is the default collection, else
+#' the name of the collection where to store the data object
+#' @param name name of the data object, defaults to the basename of
+#' the path given by the file argument
+#' @param overwrite overwrite existing data objects, default to FALSE
+#'
+#'
+#collection
+
+ri_put <- function (filename,collection=NULL, name=NULL,overwrite=FALSE) {
+
+    if(!is.character(filename)) {
+        stop("ri_put: filename is not character")
+    }
+
+    if(is.null(collection)) {
+        collection <- ri_getCollection()
+    }
+
+    if(!is.character(collection)) {
+        stop("ri_put: collection is not character")
+    }
+
+    if(is.null(name)) {
+        name <- basename(filename)
+    }
+    
+    if(!is.character(name)) {
+        stop("ri_put: name is not character")
+    }
+
+    if(!is.logical(overwrite)) {
+        stop("ri_put: overwrite is not logical")
+    }
+
+    session <- getSession()
+    objpath <- paste0(collection,"/",name)
+    session$data_objects$put(filename,objpath)
+
+
+
+
+}
---
+++ tests/testthat/test-ri_put.R
@@ -0,0 +1,69 @@
+context("ri_put")
+
+
+test_that("valid arguments", {
+              expect_error(ri_put(filename=1))
+              expect_error(ri_put(filename="test",collection=1))
+              expect_error(ri_put(filename="test",name=1))
+              expect_error(ri_put(filename="test",param=1))
+
+              x <- rnorm (10)
+              fname.x <- tempfile()
+              saveRDS(x,fname.x)
+              expect_error(ri_put(filename=fname.x,collection="nonexistingcollection"))
+              unlink(fname.x)
+})
+
+test_that("simple data object storage", {
+
+              ri_session()
+              ri_setCollection(testColl)
+              session <- getSession()
+              
+              x <- rnorm (10)
+              fname.x <- tempfile()
+              saveRDS(x,fname.x)
+
+              ri_put(fname.x)
+
+              expect_true(ri_objectExists(basename(fname.x)))
+              if(ri_objectExists(basename(fname.x))) {
+                  session$data_objects$unlink(paste0(testColl,"/",basename(fname.x)))
+              }
+              destroySession()
+
+
+})
+
+
+
+test_that("specified data object storage", {
+
+              newColl <- paste(testColl,"test1",sep="/")
+              ri_session()
+              session <- getSession()
+              ri_setCollection(testColl)
+              ri_createCollection(newColl)
+              
+              x <- rnorm (10)
+              fname.x <- tempfile()
+              saveRDS(x,fname.x)
+
+              ri_put(fname.x,collection=newColl,name="test_ri-put")
+
+              expect_true(ri_objectExists("test_ri-put",collection=newColl))
+              if(ri_objectExists(basename(fname.x))) {
+                  session$data_objects$unlink(paste0(newColl,"test_ri-put"))
+              }
+
+              ri_removeCollection(newColl)
+              destroySession()
+
+
+})
+
+
+test_that("overwrite data object", {
+
+
+})
