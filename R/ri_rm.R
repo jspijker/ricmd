a26bf6c88c3db6cc53239042bf58718e2a3dff1e
spijker <job.spijker@rivm.nl>
2020-06-05 15:21:36 +0200

    Added function ri_rm

---
+++ R/ri_rm.R
@@ -0,0 +1,35 @@
+#' Delete a data object 
+#'
+#' Delete a data object from an iRODS collection
+#'
+#' @param object name of the object
+#' @param collection collection of the object, if no collection is
+#' provided it uses the default collection
+#'
+#' @return nothing
+#'
+#' This function removes a data object from an iRODS collection. If
+#' the data object does not exists, it will be silently ignored
+#'
+#' @export
+
+ri_rm <- function(object,collection=ri_getCollection()) {
+
+    if(!is.character(object)) {
+        stop("ri_rm: object is not character")
+    }
+    if(!is.character(collection)) {
+        stop("ri_rm: collection is not character")
+    }
+
+
+    session <- getSession()
+    objpath <- file.path(collection,object)
+
+    if(ri_objectExists(object,collection)) {
+        obj <- session$data_objects$unlink(objpath)
+    }
+
+
+
+}
---
+++ tests/testthat/test-ri_rm.R
@@ -0,0 +1,68 @@
+context("ri_rm")
+
+test_that("valid function arguments",{
+              
+              expect_error(ri_rm(object=1))
+              expect_error(ri_rm(object="test",collection=1))
+
+
+})
+
+test_that("rm in default collection",{
+
+              ri_session()
+              ri_setCollection(testColl)
+              session <- getSession()
+              
+              x <- rnorm (10)
+              fname.x <- tempfile()
+              objname <- basename(fname.x)
+              saveRDS(x,fname.x)
+
+              ri_put(fname.x)
+              unlink(fname.x)
+
+              expect_true(ri_objectExists(objname))
+              ri_rm(objname)
+              expect_false(ri_objectExists(objname))
+
+              # should not give an error or warning
+              ri_rm(objname)
+
+              if(ri_objectExists(objname)) {
+                  session$data_objects$unlink(paste0(testColl,"/",objname))
+              }
+              destroySession()
+
+
+})
+
+test_that("rm in default collection",{
+
+              ri_session()
+              session <- getSession()
+              thisTestColl <- file.path(testColl,"testget")
+              ri_createCollection(thisTestColl)
+              ri_setCollection(testColl)
+              
+              x <- rnorm (10)
+              fname.x <- tempfile()
+              objname <- basename(fname.x)
+              saveRDS(x,fname.x)
+
+              ri_put(fname.x,collection=thisTestColl)
+              unlink(fname.x)
+
+              expect_true(ri_objectExists(objname,collection=thisTestColl))
+              ri_rm(objname,collection=thisTestColl)
+              expect_false(ri_objectExists(objname,collection=thisTestColl))
+
+              if(ri_objectExists(objname,collection=thisTestColl)) {
+                  session$data_objects$unlink(paste0(thisTestColl,"/",objname))
+              }
+
+              ri_removeCollection(thisTestColl)
+              destroySession()
+
+
+})
