5be319faffe8d42b2bb92e9446beea743043963f
spijker <job.spijker@rivm.nl>
2020-09-25 10:38:07 +0200

    Fixed error in ri_listDataObjects
    see also 0d0ad7d81311a71

--- tests/testthat/test-ri_listDataObjects.R
+++ tests/testthat/test-ri_listDataObjects.R
@@ -25,3 +25,26 @@ test_that("correct functioning",{
               destroySession()
 
 })
+
+
+test_that("collection argument",{
+
+
+              newColl <- paste(testColl,"test1",sep="/")
+              newSubColl <- paste(testColl,"testSub1",sep="/")
+              ri_session()
+              session <- getSession()
+              ri_setCollection(testColl)
+              ri_createCollection(newColl)
+              ri_createCollection(newSubColl)
+              session$data_objects$create(paste0(newSubColl,"/testobj1"))
+
+              res <- ri_listDataObjects(collection=newSubColl)
+              expect_equal("testobj1",res)
+
+              session$data_objects$unlink(paste0(newSubColl,"/testobj1"))
+              ri_removeCollection(newSubColl)
+              ri_removeCollection(newColl)
+              destroySession()
+
+})
