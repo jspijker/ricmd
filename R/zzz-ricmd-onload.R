a15fcce26412a03bdd7ef244d2ce9f5d15cb4833
Spijker <job.spijke@rivm.nl>
2024-12-06 17:26:10 +0100

    Renamed onattach -> onload

--- R/zzz-ricmd-onload.R
+++ R/zzz-ricmd-onload.R
@@ -1,14 +1,11 @@
 #' @import reticulate
 
-.onAttach <- function(...) {
+.onLoad <- function(...) {
 
     packageStartupMessage("loading ricmd python3 environment\nuse 'reticulate::py_config()' to see information about your python environment")
 
     pybinary <- Sys.which("python3")
     reticulate::use_python(pybinary,required=TRUE)
     reticulate::py_config()
-
     assign("onAttach_success", reticulate::py_available(), envir = .ricmdEnv)
-
-
 }
